729fe2e0b7309d15b085c7e197c0103a504c9cd7
Vitaliy Kolesnyk <vitalikkolesnuk1@gmail.com>
2016-03-28 23:42:50 +0300

    Homework (Lesson2)

---
+++ common.js
@@ -0,0 +1,36 @@
+// Задание 3
+
+//var login = 'admin';
+//var password = 'passw0rd'
+//
+//var userLogin = prompt('Введите Ваш логин');
+//
+//if( userLogin == login ){
+//    var userPass = prompt( 'Введите Ваш пароль' );
+//    if( userPass == password ){
+//        alert( 'Welcome home!' );
+//    } else if ( userPass === null ){
+//        alert( 'Canceled' );
+//    }
+//    else if ( password != userPass ){
+//        alert( 'Wrong password' );
+//    }
+//} else if ( userLogin === null ){
+//    alert( 'Canceled' );
+//} else if ( userLogin != login ) {
+//    alert( 'Access denied' );
+//}
+
+// Задание 4
+//a
+    //var a = 1; var b = 2;
+    //
+    //var msg = ( a+ b >= 3 ) ? 'Yep' : 'Noup';
+    //alert ( msg );
+//b
+//var name = '';
+//
+//var msg =  ( name == 'admin' ) ? 'Hi':
+//        ( name == 'manager' ) ? 'Hello' :
+//        ( name == '' ) ? 'No login' : '';
+//alert ( msg );
