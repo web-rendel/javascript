ea01cbd03d9d410f024916c861e41b8e0b11ab8c
Vitaliy Kolesnuk <vitalikkolesnuk1@gmail.com>
2016-03-30 19:04:52 +0300

    end lesson3

---
+++ js/lesson3.js
@@ -0,0 +1,22 @@
+// Задание 1
+// function summ (num1, num2){
+//   return num1 + num2;
+// }
+// console.log(summ(5,5));
+
+// Задание 2
+
+// function min(num1, num2) {
+//   if ( num1 < num2 ){
+//     return num1;
+//   } else {
+//     return num2;
+//   }
+// }
+//
+// console.log(min(3,-1));
+
+function sayHi() {
+  console.log( 'Hello' );
+}
+console.log(sayHi());
