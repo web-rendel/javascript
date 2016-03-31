11db8c67dba43b813c57db76ca32d148f69281ca
Vitaliy Kolesnuk <vitalikkolesnuk1@gmail.com>
2016-03-31 18:56:38 +0300

    lesson4

--- js/lesson3.js
+++ js/lesson3.js
@@ -26,17 +26,51 @@
 
 /* ****Домашнее задание 3 и 4**** */
 
-var i;
+//var i;
+//
+//for(i=1; i <=100; i++){
+//
+//  if( i % 3 === 0 && i % 5 === 0 ){
+//    console.log('FizzBuzz');
+//  } else if ( i % 3 === 0 ) {
+//    console.log('Fizz');
+//  }else if ( i % 5 === 0 ) {
+//    console.log('Buzz');
+//  }else{
+//    console.log(i)
+//  }
+//}
 
-for(i=1; i <=100; i++){
 
-  if( i % 3 === 0 && i % 5 === 0 ){
-    console.log('FizzBuzz');
-  } else if ( i % 3 === 0 ) {
-    console.log('Fizz');
-  }else if ( i % 5 === 0 ) {
-    console.log('Buzz');
-  }else{
-    console.log(i)
+/* ****Домашнее задание 5**** */
+//var board = '';
+//var line = '';
+//var tr = 3;
+//var td = 3;
+//for(var i = 0; i<tr; i++){
+//  if(i%2===0){
+//    line = '#';
+//  }else{
+//    line = '$';
+//  }
+//  for (var j = 0; j<td-1; j++){
+//    if(line[j]=== '$'){
+//      line += '#';
+//    }else{
+//      line += '$';
+//    }
+//  }
+//  board += line ;
+//}
+//console.log(board);
+
+/* ****Домашнее задание 6**** */
+function pow(x,n){
+  var result = x;
+  for (var i = 1; i<n; i++){
+   result = result * x;
   }
+  return result;
 }
+console.log(pow(3,3));
+
