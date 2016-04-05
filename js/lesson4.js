9b890a6fc7cbdb7ae6aa048c4748032eaf49df1d
Vitaliy Kolesnuk <vitalikkolesnuk1@gmail.com>
2016-04-05 19:00:56 +0300

    lesson 05.04

--- js/lesson4.js
+++ js/lesson4.js
@@ -1,9 +1,27 @@
-function sumTo(n){
-    if (n != 1){
-        return n + sumTo( n - 1 );
-    }else {
-        return n;
-    }
-}
+//function sumTo(n){
+//    if (n != 1){
+//        return n + sumTo( n - 1 );
+//    }else {
+//        return n;
+//    }
+//}
+//console.log(sumTo(100));
 
-console.log(sumTo(100));
+
+//function superrandom1 (max, min){
+//    console.log(Math.floor(Math.random() * (max - min + 1)+ min));
+//}
+//superrandom1(10,20);
+
+
+//function superrandom2 (max){
+//    console.log(Math.floor(Math.random() * (max - 0 + 1)+ 0));
+//}
+//superrandom2(20);
+
+
+function  upperFirst(str){
+    var first = str.charAt(0);
+    console.log(first.toUpperCase() + str.slice(1));
+}
+upperFirst('welcome');
