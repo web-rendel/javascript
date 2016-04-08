4a9e028aa37bfaa257a895fc1279940eacc30b42
Vitaliy Kolesnuk <vitalikkolesnuk1@gmail.com>
2016-04-08 18:46:47 +0300

    lesson5 start

--- js/lesson5.js
+++ js/lesson5.js
@@ -1,1 +1,30 @@
+// **** Задачка 1
 
+//var user = {};
+//user.name = 'Sergey';
+//user.surname = 'Petrov';
+//user.name = 'Andrey';
+//user.age = 30;
+//delete user.name;
+//
+//console.log(user);
+
+// **** Задачка 2
+
+
+
+var user = {  }; 
+
+for (var prop in user) { 
+    if (prop){
+        console.log( '1' );
+    }
+}
+
+//function isEmpty (obj){
+//
+//}
+//var todoList = {};
+//console.log( isEmpty(todoList) );
+//todoList['homework'] = 'lesson5';
+//console.log( isEmpty(todoList) );
