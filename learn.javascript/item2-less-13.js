0f939ec376adcda580d5efdaebbeb378771cd413
Vitaliy Kolesnyk <vitalikkolesnuk1@gmail.com>
2016-07-27 23:24:20 +0300

    less13

---
+++ learn.javascript/item2-less-13.js
@@ -0,0 +1,22 @@
+// Проверка if внутри диапазона
+age = 90;
+if( age >= 15 && age <= 90 ){
+	alert("Cool!");
+}else{
+	alert("Bad!!!")
+}
+
+// Проверка if вне диапазона
+/*age = 91;
+if( !(age >= 15 && age <= 90) ){
+	alert("Cool!");
+}else{
+	alert("Bad!!!")
+}*/
+
+/*age = 14;
+if( age < 15 || age > 90 ){
+	alert("Cool!");
+}else{
+	alert("Bad!!!")
+}*/
